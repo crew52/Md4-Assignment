c95236039afd0467577f27614fb9e807368e1433
crew52 <dungnc150602@gmail.com>
2025-03-01 19:44:55 +0700

    update list type

--- src/main/java/codegym/c10/assignment/controller/TypeController.java
+++ src/main/java/codegym/c10/assignment/controller/TypeController.java
@@ -1,5 +1,9 @@
 package codegym.c10.assignment.controller;
 
+import codegym.c10.assignment.model.Type;
+import codegym.c10.assignment.service.IComputerService;
+import codegym.c10.assignment.service.ITypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +13,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/types")
 public class TypeController {
+    @Autowired
+    private IComputerService computerService;
+
+    @Autowired
+    private ITypeService typeService;
+
     @GetMapping
-    public ModelAndView listType(Pageable pageable) {
+    public ModelAndView listProvince() {
         ModelAndView modelAndView = new ModelAndView("/type/index");
+        Iterable<Type> types = typeService.findAll();
+        modelAndView.addObject("types", types);
         return modelAndView;
     }
 }
