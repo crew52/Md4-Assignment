8477efc88a2d0e8526d72729749fd19eebef0d20
crew52 <dungnc150602@gmail.com>
2025-03-01 11:52:38 +0700

    update add computer

--- src/main/java/codegym/c10/assignment/controller/ComputerController.java
+++ src/main/java/codegym/c10/assignment/controller/ComputerController.java
@@ -8,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -47,5 +45,21 @@ public class ComputerController {
         ModelAndView modelAndView = new ModelAndView("/computer/index");
         modelAndView.addObject("computers", computers);
         return modelAndView;
+    }
+
+
+    @GetMapping("/create")
+    public ModelAndView createForm() {
+        ModelAndView modelAndView = new ModelAndView("/computer/create");
+        modelAndView.addObject("computer", new Computer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute("computer") Computer computer,
+                         RedirectAttributes redirectAttributes) {
+        computerService.save(computer);
+        redirectAttributes.addFlashAttribute("message", "Create new computer successfully");
+        return "redirect:/computers";
     }
 }
