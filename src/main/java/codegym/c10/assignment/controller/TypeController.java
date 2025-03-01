2c180be4d23c6370a91c72a43b15ac4038e6eac1
crew52 <dungnc150602@gmail.com>
2025-03-01 20:00:15 +0700

    done add types

--- src/main/java/codegym/c10/assignment/controller/TypeController.java
+++ src/main/java/codegym/c10/assignment/controller/TypeController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/types")
@@ -25,5 +28,21 @@ public class TypeController {
         Iterable<Type> types = typeService.findAll();
         modelAndView.addObject("types", types);
         return modelAndView;
+    }
+
+
+    @GetMapping("/create")
+    public ModelAndView createForm() {
+        ModelAndView modelAndView = new ModelAndView("/type/create");
+        modelAndView.addObject("type", new Type());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute("type") Type type,
+                         RedirectAttributes redirectAttributes) {
+        typeService.save(type);
+        redirectAttributes.addFlashAttribute("message", "Create new type successfully");
+        return "redirect:/types";
     }
 }
